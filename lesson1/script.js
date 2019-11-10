38089ed01fad8b2b8f5a4b885fb96045658110e0
Jamilya <jamilyam312@gmail.com>
2019-11-10 22:33:36 +0600

    "third homework"

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,5 +1,14 @@
-let money = + prompt("Ваш бюджет на месяц?",''),
+let money,
+    time;
+function start() {
+    money = + prompt("Ваш бюджет на месяц?",'');
     time = prompt("Введите дату в формате YYYY-MM-DD",'');
+
+    while (isNaN(money) || money == '' || money == null) {
+        money = + prompt("Ваш бюджет на месяц?",'');
+    }
+}
+start();
 
 
 let appData = {
@@ -8,8 +17,65 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
+
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Введите статью расходов в этом месяце', ''),
+            b = prompt('Во сколько обойдется?', '');
+
+        if ((typeof (a)) ==='string' && (typeof (a)) != null && (typeof (b)) != null
+           && a != '' && b != '' && a.length < 50) {
+           console.log('done');
+           appData.expenses[a] = b;
+        } else {
+          console.log('Error');
+          i--;
+        }
+    }
+}
+chooseExpenses();
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt('How much you have in your savings?');
+        let percent = +prompt("On what percent?");
+
+        appData.monthIncome = save / 100 /12*percent;
+        alert('Your monthly income from your deposit is' + appData.monthIncome);
+    }
+}
+checkSavings();
+
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "som");
+}
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log('Minimal level of income');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log('Average level of income');
+    } else if (appData.moneyPerDay > 2000) {
+        console.log('High level of income');
+    } else {
+        console.log('Error');
+    }
+}
+detectLevel();
+
+function chooseOptExpenses() {
+    for (let i = 0; i <= 3; i++) {
+        q = prompt('Статья необязательных расходов?', '');
+        appData.optionalExpenses[i] = q;
+        console.log(appData.optionalExpenses);
+    }
+}
+chooseOptExpenses();
+
 
 
 // for (let i = 0; i < 2; i++) {
@@ -43,34 +109,33 @@ let appData = {
 // };
 
 //do while
-let i =0;
-do {
-    let a = prompt('Введите статью расходов в этом месяце', ''),
-        b = prompt('Во сколько обойдется?', '');
+// let i =0;
+// do {
+//     let a = prompt('Введите статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+//
+//     if ( typeof (a) === 'string' && typeof (a) != null && typeof (b) != null
+//         && a != '' && b != '' && a.length < 50 ) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log('Error');
+//         i--;
+//     }
+//     i++;
+// }
+// while (i < 2);
 
-    if ( typeof (a) === 'string' && typeof (a) != null && typeof (b) != null
-        && a != '' && b != '' && a.length < 50 ) {
-        console.log('done');
-        appData.expenses[a] = b;
-    } else {
-        console.log('Error');
-        i--;
-    }
-    i++;
-}
-while (i < 2);
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert('Everyday budget:' + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-    console.log('Minimal level of income');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('Average level of income');
-} else if (appData.moneyPerDay > 2000) {
-    console.log('High level of income');
-} else {
-    console.log('Error');
-};
-
+// appData.moneyPerDay = (appData.budget / 30).toFixed();
+//
+// alert('Everyday budget:' + appData.moneyPerDay);
+//
+// if (appData.moneyPerDay < 100) {
+//     console.log('Minimal level of income');
+// } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+//     console.log('Average level of income');
+// } else if (appData.moneyPerDay > 2000) {
+//     console.log('High level of income');
+// } else {
+//     console.log('Error');
+// };
